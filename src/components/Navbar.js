274fb36db6fe22297ab75e1e6322f2368d7b7f32
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2023-10-14 23:00:53 +0530

    included proptypes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,3 +57,12 @@ export default function Navbar(props) {
   );
 }
 
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: "Set the title",
+  aboutText: "Set about text",
+};
